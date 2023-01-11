726140a75193e034d10ca877f08ce4da8e398866
Andres <nanoandres_24@hotmail.com>
2023-01-11 22:11:16 +0100

    rough version 1 of our models and training in pl

--- Experiment/pl_model.py
+++ Experiment/pl_model.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
@@ -8,19 +9,22 @@ from transformers import AutoModelForTokenClassification, AutoConfig, AutoTokeni
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
 import copy
+import evaluate
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 from datasets import Dataset
+from tqdm import tqdm
 
 import sys
 import os
 sys.path.append('../')
-from util.utils import get_tag_mappings, get_data, compute_metrics, compute_metrics_crf
+from util.utils import get_tag_mappings, get_data
 from util.dataloader import PreDataCollator
 
 SEED = 42
 pl.seed_everything(SEED, workers=True)
+
 
 class LightninggarNER(pl.LightningModule):
 
@@ -30,44 +34,61 @@ class LightninggarNER(pl.LightningModule):
         super().__init__()
         self.total_size_train = total_size_train
         self.save_hyperparameters()
-        #TODO: should comment the following 4 lines and replace with compute_metrics
-        self.train_acc = torchmetrics.Accuracy(task='multiclass', num_classes=num_labels)
-        self.val_acc = torchmetrics.Accuracy(task='multiclass', num_classes=num_labels)
-        self.train_f1 = MulticlassF1Score(num_classes=num_labels)
-        self.val_f1 = MulticlassF1Score(num_classes=num_labels)
+
+        self.metric_acc = evaluate.load("accuracy")
+        self.metric_f1 = evaluate.load("f1")
+        self.my_metrics = dict()
+
         self.config = AutoConfig.from_pretrained(model_name, num_labels=num_labels)
         #self.model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=number_of_labels) 
         self.model = AutoModelForTokenClassification.from_pretrained(model_name, config=self.config) 
-
         self.data_collator = DataCollatorForTokenClassification(tokenizer, return_tensors='pt')
     
     def forward(self, **inputs):
         return self.model(**inputs)
     
     def common_step(self, batch, batch_idx):
+        labels = batch["labels"]
         outputs = self(**batch)
+        self.pl_compute_metrics(outputs.logits, labels)  # we don't need to call this for every step? every epoch instead? idk
         return outputs
-    
+
+    def pl_compute_metrics(self, logits, labels):
+        logits = logits.cpu()
+        labels = labels.cpu()
+        pred_ids = torch.flatten(torch.argmax(logits, axis=-1), 0)  # stack instead of flatten?
+        labels = torch.flatten(labels, 0)
+        mask = labels != -100
+        clean_labels = torch.masked_select(labels, mask)
+        clean_preds = torch.masked_select(pred_ids, mask)
+        acc = self.metric_acc.compute(predictions=clean_preds, references=clean_labels)
+        f1 = self.metric_f1.compute(predictions=clean_preds, references=clean_labels, average='macro')
+        #print({'accuracy': acc['accuracy'], 'f1':f1['f1']})
+        self.my_metrics = {'accuracy': acc['accuracy'], 'f1':f1['f1']}
+
+
     #TODO: do I need to reset after end of epoch each metric?, and add logger argumnent to log
     def training_step(self, batch, batch_idx):
         outputs = self.common_step(batch, batch_idx)
         loss = outputs.loss
-        #self.log('train_acc', self.train_acc, on_step=True, on_epoch=True, prog_bar=True)
-        #self.log('train_f1', self.train_f1, on_step=True, on_epoch=True, prog_bar=True)
-        #self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        return loss
+    
+    def training_epoch_end(self, outs):
+        self.log("train_acc_epoch", self.my_metrics['accuracy'], on_epoch=True, prog_bar=True)
+        self.log("train_f1_epoch", self.my_metrics['f1'], on_epoch=True, prog_bar=True)
+        
 
     def validation_step(self, batch, batch_idx):
-        loss, logits = self.common_step(batch, batch_idx)
-        #self.log('val_acc', self.val_acc, on_step=True, on_epoch=True, prog_bar=True)
-        #self.log('val_f1', self.val_f1, on_step=True, on_epoch=True, prog_bar=True)
-        #self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        outputs = self.common_step(batch, batch_idx)
+        loss = outputs.loss
+        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_epoch_end(self, outs):
+        self.log("val_acc_epoch", self.my_metrics['accuracy'], on_epoch=True, prog_bar=True)
+        self.log("val_f1_epoch", self.my_metrics['f1'], on_epoch=True, prog_bar=True)
     
-    #def train_dataloader(self):
-    #    return DataLoader(self.train_data, collate_fn=self.data_collator)
-
-    #def val_dataloader(self):
-    #    return DataLoader(self.dev_data, collate_fn=self.data_collator)
-
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.hparams.lr)
         #optimizer = AdamW(self.parameters(), lr=1e-04)
@@ -104,8 +125,8 @@ dev_tokenized = dev_data.map(collator, remove_columns=dev_data.column_names, bat
 
 
 data_collator = DataCollatorForTokenClassification(tokenizer, return_tensors='pt')
-train_dataloader = DataLoader(train_tokenized, collate_fn=data_collator)
-val_dataloader = DataLoader(dev_tokenized, collate_fn=data_collator)
+train_dataloader = DataLoader(train_tokenized, batch_size=8, collate_fn=data_collator)
+val_dataloader = DataLoader(dev_tokenized, batch_size=8, collate_fn=data_collator)
 # saves top-K checkpoints based on "val_loss" metric
 checkpoint_callback = ModelCheckpoint(
     every_n_train_steps=1000,    
@@ -116,7 +137,7 @@ checkpoint_callback = ModelCheckpoint(
     filename="sample-mnist-{epoch:02d}-{val_loss:.2f}",
 )
 
-max_epochs = 7
+max_epochs = 3
 fast_garner = LightninggarNER("distilbert-base-uncased", num_train_epochs=max_epochs,
                               num_labels=num_of_labels, total_size_train=total_size_train)
 
@@ -125,6 +146,7 @@ ckp_path = './lightning_model'
 trainer = pl.Trainer(
            max_epochs=max_epochs,
            min_epochs=1,
+           log_every_n_steps=500,
            default_root_dir=ckp_path,
            enable_checkpointing=True,
            accumulate_grad_batches=2,
